3ba2537129cd9c966b959359b88f5eb640198204
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-09-02 23:50:45 +0300

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -8,3 +8,10 @@ def narcissistic(value):
         return True
     else:
         return False
+
+#https://www.codewars.com/kata/551dd1f424b7a4cdae0001f0/train/python
+def who_is_next(names, r):
+    n = len(names)
+    while r > n:
+        r = (r - n + 1) >> 1
+    return names[r - 1]
