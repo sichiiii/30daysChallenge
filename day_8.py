7c46f6f76ffcba7fac90a08ae421bcc7c43cbd7b
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2021-09-09 19:16:28 +0300

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -42,3 +42,11 @@ def accum(s):
             result+=s[i].lower()
         result+='-'
     return result[:-1]
+
+#https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+def solution(number):
+    result = 0
+    for i in range(1, number):
+        if i % 3 == 0 or i % 5 == 0:
+            result+=i
+    return result
