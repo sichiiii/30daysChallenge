9f44ad9f6c20cc33762a2527f0515fe36df68db8
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2021-09-12 00:15:04 +0300

    Update day_10.py

--- day_10.py
+++ day_10.py
@@ -8,3 +8,15 @@ def is_prime(n):
             return False
         i += 1
     return True
+
+#https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
+def order(sentence):
+    sent_list = sentence.split()
+    result = ''
+    count = 1
+    for i in range(0, len(sent_list)):
+        for j in range(0 ,len(sent_list)):
+            if str(count) in sent_list[j]:
+                result+=sent_list[j]+' '
+        count+=1
+    return result[:-1]
