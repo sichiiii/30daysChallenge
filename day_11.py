5dc135cf2b4efa0c470d5b617e859517624d6352
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2021-09-13 23:16:10 +0300

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -18,3 +18,25 @@ def match_words(acronym,words):
             if words[j][0] == i:
                 result[i] = words[j]
     return result
+
+#https://www.codewars.com/kata/5f6787948a3830002fc40077/train/python      #trash
+def expand_usernames(data): 
+    result = []
+    buffer = []
+    for i in range(0, len(data)):
+        spl = data[i][0].split(',')
+        if len(spl) > 0:
+            for j in spl:
+                #j = j.replace(' ', '')
+                if j != '' and j != ' ' and j != '   ':
+                    if j[0] == ' ':
+                        j = j[1:]
+                    if j[-1] == ' ':
+                        j = j[:-1]
+                    if j[-1] == ' ':
+                        j = j[:-1]
+                    buffer = [j, data[i][1]] 
+                    result.append(buffer)
+                    buffer = []
+        else:
+            result.append(data[i])
