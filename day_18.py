ddcb157e2b8c501d906408a87ab61ab708735357
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2021-09-23 19:21:43 +0300

    Update day_18.py

--- day_18.py
+++ day_18.py
@@ -20,3 +20,20 @@ def encrypt_this(text):
             result+=new
             result+=' '
         return result[:-1]
+
+#https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
+def likes(names):
+    if names == []:
+        result = 'no one'
+    elif len(names) == 1:
+        result = names[0]
+    elif len(names) == 2:
+        result = names[0] + ' and ' + names[1]
+        return result+' like this'
+    elif len(names) == 3:
+        result = names[0] + ', ' + names[1] + ' and ' +names[2]
+        return result+' like this'
+    else:
+        result = names[0] + ', ' + names[1] + ' and ' + str(len(names)-2) + ' others'
+        return result+' like this' 
+    return result+' likes this'
