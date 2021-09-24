d11bd8759ab7182dc2ae9c3aadca702d8d221d9b
cyberjabka <76742792+sichiiii@users.noreply.github.com>
2021-09-24 09:38:40 +0300

    Update day_19.py

--- day_19.py
+++ day_19.py
@@ -13,3 +13,17 @@ def calculator(txt):
     for i in range(0, count):
         result += '.'
     return result
+
+#https://www.codewars.com/kata/60f94b4c8b940b0038f5e237/train/python
+def boom_intensity(n):
+    if n == 1:
+        return 'boom'
+    result = 'B'
+    for i in range(n):
+        result += 'o'
+    result +='m'
+    if n % 2 == 0:
+        result += '!'
+    if n % 5 == 0:
+        result = result.upper()
+    return result
